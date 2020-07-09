dbd7fb7c10b36bbe9bc42b3da5261353eebd956a
Lars Koester <lkoester2015@fau.edu>
2020-07-09 17:30:29 -0400

    added auth update

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
         <h1>We now have Auth!</h1>
       </header>
